ddc7f32aedf6d72cab6c3a6897ebfb36253e33d5
Manuel Barbero <emanbarval@gmail.com>
2023-10-12 23:48:38 +0200

    standalone-card-working

--- src/components/CardCat.js
+++ src/components/CardCat.js
@@ -1,8 +1,12 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function CardCat({name, image, status, species, gender}) {
+function CardCat({id, name, image, status, species, gender}) {
   return (
+    
+    <Link to={`/profile/${id}`}>
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={image} />
       <Card.Body>
@@ -12,6 +16,7 @@ function CardCat({name, image, status, species, gender}) {
         </Card.Text>
       </Card.Body>
     </Card>
+    </Link>
   );
 }
 
