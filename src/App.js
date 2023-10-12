229b56391b4382a00fb251f2aa26372eaa3500b0
Manuel Barbero <emanbarval@gmail.com>
2023-10-12 20:54:05 +0200

    basic-gallery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import Catalogo from "./components/Catalog";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{maxHeight:'90hv', boxSizing:'border-box'}}>
+      <h1 className="text-center">Rick & Morty</h1>
+      <Catalogo />
     </div>
   );
-}
+};
 
 export default App;
